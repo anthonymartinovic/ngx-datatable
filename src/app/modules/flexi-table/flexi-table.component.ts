ece911a213877fd77d8f421b2b132398c5e9c597
anthonymartinovic <anthony_5950@hotmail.com>
2018-02-28 22:55:09 +1100

    angular-cli conversion: css -> scss

--- src/app/modules/flexi-table/flexi-table.component.ts
+++ src/app/modules/flexi-table/flexi-table.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-flexi-table',
   templateUrl: './flexi-table.component.html',
-  styleUrls: ['./flexi-table.component.css']
+  styleUrls: ['./flexi-table.component.scss']
 })
 export class FlexiTableComponent implements OnInit {
 
